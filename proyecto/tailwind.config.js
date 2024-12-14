f79065134d245455ad8f0b3222cb8b97e4a423d3
Cris20034 <cris.vasq2003@hotmail.com>
2024-12-14 18:17:07 -0500

    pequeños cambios

--- proyecto/tailwind.config.js
+++ proyecto/tailwind.config.js
@@ -1,13 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -17,3 +8,4 @@ module.exports = {
   },
   plugins: [],
 }
+
